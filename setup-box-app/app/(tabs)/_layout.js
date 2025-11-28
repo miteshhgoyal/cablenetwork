6836bc8a2f9d60f0656d433da4dcf93093a6f00c
Mitesh Goyal <miteshgoyal.00@gmail.com>
2025-11-28 10:32:34 +0530

    removed expiry.js file

--- setup-box-app/app/(tabs)/_layout.js
+++ setup-box-app/app/(tabs)/_layout.js
@@ -41,13 +41,6 @@ export default function TabsLayout() {
                     ),
                 }}
             />
-
-            <Tabs.Screen
-                name="expired"
-                options={{
-                    href: null,
-                }}
-            />
         </Tabs>
     );
 }
